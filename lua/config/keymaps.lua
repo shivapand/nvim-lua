3eb39135415788799a7ff93b1ef3f2c19dab0479
shivapand <shiva.pandit@ecolibrium.io>
2025-06-24 12:08:28 +0530

    removed nocie as conflicts with prettier

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set({ 'i', 'n' }, '<C-s>', '<ESC>:w<CR>', { desc = 'Save file' })
+vim.keymap.set({ 'i', 'n' }, '<C-s>', '<ESC>:silent w<CR>', { desc = 'Save file' })
 
 vim.keymap.set('n', '<Leader>j', ':execute "tabmove" tabpagenr() - 2<CR>', { desc = 'Move tab prev' })
 vim.keymap.set('n', '<Leader>k', ':execute "tabmove" tabpagenr() + 1<CR>', { desc = 'Move tab next' })
