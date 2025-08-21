76ae17dd36bf449b0ecb1f1426ff08e2ee488b3a
shivapand <shiva.pandit@ecolibrium.io>
2025-08-21 09:38:11 +0530

    lsp on top

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,17 +1,28 @@
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
-vim.opt.number = true
-
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
+vim.opt.termguicolors = true
 
 vim.opt.clipboard='unnamedplus'
 
-vim.g.loaded_matchparen = true
-
 vim.opt.swapfile = false
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+vim.o.showtabline = 2
+vim.opt.number = true
+vim.opt.signcolumn = "yes"
 vim.opt.showmode = false
+
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath('data') .. '/undo'
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.virtualedit = 'block'
+vim.opt.inccommand = 'split'
