474929a8cf46abd171cc6f92b10265b6a3ca8781
shivapand <shiva.pandit@ecolibrium.io>
2025-08-25 12:01:30 +0530

    oil pwd autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,15 +1,15 @@
 vim.opt.updatetime = 500
 
-vim.api.nvim_create_augroup("LspSagaDiagnostics", { clear = true })
-
-vim.api.nvim_create_autocmd("CursorHold", {
-  group = "LspSagaDiagnostics",
+vim.api.nvim_create_autocmd('CursorHold', {
+  group = vim.api.nvim_create_augroup('LspSagaDiagnostics', { clear = true }),
   command = 'Lspsaga show_cursor_diagnostics ++unfocus',
-  desc = "Show LSP diagnostics on cursor hold",
+  desc = 'Show LSP diagnostics on cursor hold',
 })
 
-vim.api.nvim_create_autocmd("TabNew", {
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = vim.api.nvim_create_augroup('OilRelativePathFix', { clear = true }),
   callback = function()
-    vim.cmd("cd `pwd`")
-  end
+    vim.cmd('cd `pwd`')
+  end,
+  desc = 'Fix for Oil opening files with absolute-path'
 })
