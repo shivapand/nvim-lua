ca8b0339a767abc44ebb4e0e434cb5a3c3f9e80b
shivapand <shiva.pandit@ecolibrium.io>
2025-09-05 20:08:22 +0530

    complete chatgpt persist it and restore from filesystem

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,5 @@
 vim.opt.updatetime = 500
+
 vim.api.nvim_create_autocmd(
   'CursorHold',
   {
@@ -10,6 +11,7 @@ vim.api.nvim_create_autocmd(
     desc = 'Show LSP diagnostics on cursor hold',
   }
 )
+
 
 vim.api.nvim_create_autocmd(
   'BufReadPost',
@@ -27,6 +29,7 @@ vim.api.nvim_create_autocmd(
 
 
 vim.opt.sessionoptions:append('globals')
+
 vim.api.nvim_create_autocmd(
   'User',
   {
@@ -41,22 +44,55 @@ vim.api.nvim_create_autocmd(
 )
 
 
-local buf_views = {}
+-- Path to save views
+local view_file = vim.fn.stdpath("data") .. "/scroll_positions.json"
+
+-- Load views from file
+local function load_views()
+  local f = io.open(view_file, "r")
+  if not f then return {} end
+  local ok, data = pcall(vim.json.decode, f:read("*a"))
+  f:close()
+  return ok and data or {}
+end
+
+-- Save views to file
+local function save_views(views)
+  local f = io.open(view_file, "w")
+  if not f then return end
+  f:write(vim.json.encode(views))
+  f:close()
+end
+
+-- Keep in-memory table of views
+local buf_views = load_views()
+
+-- Save position when leaving a buffer
 vim.api.nvim_create_autocmd("BufLeave", {
   callback = function()
     local buf = vim.api.nvim_get_current_buf()
-
-    buf_views[buf] = vim.fn.winsaveview()
+    local name = vim.api.nvim_buf_get_name(buf)
+    if name ~= "" then
+      buf_views[name] = vim.fn.winsaveview()
+    end
   end,
 })
+
+-- Restore position when entering a buffer
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     local buf = vim.api.nvim_get_current_buf()
-
-    local view = buf_views[buf]
-
+    local name = vim.api.nvim_buf_get_name(buf)
+    local view = buf_views[name]
     if view then
       vim.fn.winrestview(view)
     end
+  end,
+})
+
+-- Save everything on exit
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  callback = function()
+    save_views(buf_views)
   end,
 })
