002b69ca68b028192edff6ee747f8c56a573e8db
shivapand <shiva.pandit@ecolibrium.io>
2025-09-19 00:22:08 +0530

    done for today

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,8 +6,10 @@ return {
       options = {
         mode = "tabs",
         numbers = "ordinal",
+        show_close_icon = false,
         show_buffer_close_icons = false,
-        diagnostics = 'coc'
+        diagnostics = 'coc',
+        diagnostics_update_in_insert = true
       }
     }
   end
