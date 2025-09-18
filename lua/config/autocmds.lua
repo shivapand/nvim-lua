fe4f0f5ebdcc24973faa12cb8ef86a94512ac129
shivapand <shiva.pandit@ecolibrium.io>
2025-09-18 16:38:21 +0530

    back to coc started

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,19 +1,6 @@
 vim.opt.updatetime = 500
 
 vim.api.nvim_create_autocmd(
-  'CursorHold',
-  {
-    group = vim.api.nvim_create_augroup(
-      'LspSagaDiagnostics',
-      { clear = true }
-    ),
-    command = 'Lspsaga show_cursor_diagnostics ++unfocus',
-    desc = 'Show LSP diagnostics on cursor hold',
-  }
-)
-
-
-vim.api.nvim_create_autocmd(
   'BufReadPost',
   {
     group = vim.api.nvim_create_augroup(
