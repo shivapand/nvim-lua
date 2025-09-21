7ab3599aa6aafabf31c5784787d4b7d8399a202b
shivapand <shiva.pandit@ecolibrium.io>
2025-09-21 21:04:01 +0530

    made bufferline diagnostics lsp, and removed on-insert for it

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,8 +8,7 @@ return {
         numbers = "ordinal",
         show_close_icon = false,
         show_buffer_close_icons = false,
-        diagnostics = 'coc',
-        diagnostics_update_in_insert = true,
+        diagnostics = 'nvim_lsp',
         show_buffer_icons = true
       }
     }
