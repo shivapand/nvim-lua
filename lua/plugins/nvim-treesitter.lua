fbc8411f3e427e4ac0c0d7136afb105a7f8557ab
shivapand <shiva.pandit@ecolibrium.io>
2025-09-27 14:24:33 +0530

    gemini added "javascript", "typescript" to tree-sitter autoload

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
   },
   config = function()
     require 'nvim-treesitter.configs'.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript" },
 
       sync_install = false,
 
