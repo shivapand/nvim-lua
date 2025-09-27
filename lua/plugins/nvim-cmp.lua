7640ca434826b0f3c22fe502073d7f48f54b18e1
shivapand <shiva.pandit@ecolibrium.io>
2025-09-27 14:23:15 +0530

    nvim-cmp up-down tabs refactored by gemini

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,50 +8,24 @@ return {
   },
   config = function()
     local cmp = require("cmp")
-
     cmp.setup({
       mapping = {
         -- Confirm with Enter (like coc.nvim)
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
         -- Tab / Shift-Tab to cycle items
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            fallback()
-          end
-        end, { "i", "c" }),
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            fallback()
-          end
-        end, { "i", "c" }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 
         -- Arrow keys to cycle items (like coc.nvim)
-        ["<Down>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            fallback()
-          end
-        end, { "i", "c" }),
+        ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 
-        ["<Up>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            fallback()
-          end
-        end, { "i", "c" }),
+        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 
         -- Ctrl+Space to trigger completion (like coc.nvim)
         ["<C-Space>"] = cmp.mapping.complete(),
       },
-
       sources = cmp.config.sources({
         {
           name = "nvim_lsp",
