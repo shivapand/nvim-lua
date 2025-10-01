0dc57a990484be14775224feb1d77f817570ecf4
shivapand <shiva.pandit@ecolibrium.io>
2025-10-01 16:36:44 +0530

    nvim-cmp bare-minimum

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,56 +4,46 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-emmet",
+    "hrsh7th/cmp-path"
   },
   config = function()
     local cmp = require("cmp")
 
     cmp.setup({
       mapping = {
-        -- Confirm with Enter (like coc.nvim)
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-        -- Tab / Shift-Tab to cycle items
-        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-
-        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-
-        -- Arrow keys to cycle items (like coc.nvim)
-        ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-
-        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-
-        -- Ctrl+Space to trigger completion (like coc.nvim)
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
       },
-
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "emmet" },
+      sources = {
+        {
+          name = "nvim_lsp",
+          entry_filter = function(entry)
+            local ft = vim.bo.filetype
+            if entry.source.name == "nvim_lsp"
+                and entry.completion_item.detail == "Emmet Abbreviation"
+                and (ft == "javascript" or ft == "javascriptreact"
+                  or ft == "typescript" or ft == "typescriptreact") then
+              return false
+            end
+            return true
+          end,
+        },
         {
           name = "buffer",
           option = {
             get_bufnrs = function()
-              local bufs = {}
-              for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-                if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
-                  table.insert(bufs, buf)
-                end
-              end
-              return bufs
-            end
-          }
+              return vim.tbl_filter(function(buf)
+                return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
+              end, vim.api.nvim_list_bufs())
+            end,
+          },
         },
-        { name = "path" },
-      }),
-
-      -- Behave like coc.nvim
-      preselect = cmp.PreselectMode.Item,
-      completion = {
-        completeopt = "menu,menuone,noinsert",
+        { name = "path" }
       },
+      preselect = cmp.PreselectMode.Item,
+      completion = { completeopt = "menu,menuone,noinsert" },
+      window = { documentation = cmp.config.window.bordered() }
     })
-  end,
+  end
 }
