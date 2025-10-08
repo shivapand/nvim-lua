a332f94ed1cb91dda45943492a378b86036f8698
shivapand <shiva.pandit@ecolibrium.io>
2025-10-08 09:33:17 +0530

    removed snippets from blink-cmp, and using rust without warnings

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,33 +1,30 @@
 return {
   "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
   version = "*",
   opts = {
     appearance = { nerd_font_variant = "mono" },
     completion = {
       documentation = { auto_show = true },
-      accept = { 
-        auto_brackets = {
-          enabled = false
-        }
-      }
+      accept = {
+        auto_brackets = { enabled = false },
+      },
     },
     keymap = {
       preset = "enter",
       ["<c-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<tab>"] = { "select_next", "snippet_forward", "fallback" },
-      ["<s-tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<tab>"] = { "select_next", "fallback" },
+      ["<s-tab>"] = { "select_prev", "fallback" },
       ["<up>"] = { "select_prev", "fallback" },
       ["<down>"] = { "select_next", "fallback" },
     },
     sources = {
-      default = { "lazydev", "path", "lsp", "snippets", "buffer" },
+      default = { "lazydev", "path", "lsp", "buffer" },
       providers = {
         lazydev = {
-          score_offset = 1,
-          min_keyword_length = 3,
           name = "LazyDev",
           module = "lazydev.integrations.blink",
+          score_offset = 1,
+          min_keyword_length = 3,
           fallbacks = {},
         },
         lsp = {
@@ -40,18 +37,14 @@ return {
           min_keyword_length = 1,
           fallbacks = {},
         },
-        snippets = {
-          score_offset = 0,
-          min_keyword_length = 3,
-          fallbacks = {},
-        },
         buffer = {
           score_offset = 2,
+          min_keyword_length = 3,
         },
       },
     },
-    fuzzy = {
-      implementation = "prefer_rust_with_warning",
+    fuzzy = 
+      implementation = "prefer_rust",
       sorts = {
         function(a, b)
           if a.client_name == 'emmet_language_server' and
@@ -61,12 +54,11 @@ return {
               b.client_name == 'emmet_language_server' then
             return true
           end
-
           return nil
         end,
         'score',
-        'sort_text'
-      }
+        'sort_text',
+      },
     },
   },
   opts_extend = { "sources.default" },
