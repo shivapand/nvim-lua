693b6240a5fc53dd7f2d2fd93479141805338f4a
shivapand <Shiva.pandit@ecolibrium.io>
2025-11-14 23:57:40 +0530

    run e! when tab-switch to refresh linter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,17 @@ vim.api.nvim_create_autocmd('CursorHold', {
 	command = 'Lspsaga show_cursor_diagnostics ++unfocus',
 	desc = 'Show LSP diagnostics on cursor hold'
 })
+
+-- Refresh buffer when switching tabs to trigger linter
+vim.api.nvim_create_autocmd('TabEnter', {
+	group = vim.api.nvim_create_augroup('RefreshOnTabSwitch', { clear = true }),
+	callback = function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		local filename = vim.api.nvim_buf_get_name(bufnr)
+		-- Only refresh file buffers that have a filename
+		if vim.bo[bufnr].buftype == '' and filename ~= '' then
+			vim.cmd('e!')
+		end
+	end,
+	desc = 'Refresh buffer when switching tabs'
+})
