d5bad11eed997d99188377fa0dc20a4960dd1229
shivapand <Shiva.pandit@ecolibrium.io>
2025-12-02 21:28:36 +0530

    re-doing nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,88 +1,26 @@
 return {
 	'hrsh7th/nvim-cmp',
+	event = 'InsertEnter',
 	dependencies = {
-		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-path',
-		'onsails/lspkind-nvim'
+		'hrsh7th/cmp-nvim-lsp'
 	},
 	config = function()
 		local cmp = require('cmp')
-		local lspkind = require('lspkind')
-		local CompletionItemKind = vim.lsp.protocol.CompletionItemKind
-
-		-- Kind priorities (use numeric enum keys)
-		local kind_priorities = {
-			[CompletionItemKind.Keyword] = 1,
-			[CompletionItemKind.Variable] = 2,
-			[CompletionItemKind.Function] = 3,
-			[CompletionItemKind.Method] = 4,
-			[CompletionItemKind.Field] = 5,
-			[CompletionItemKind.Constant] = 6,
-			[CompletionItemKind.Class] = 7,
-			[CompletionItemKind.Interface] = 8,
-			[CompletionItemKind.Module] = 9,
-			[CompletionItemKind.Property] = 10,
-			[CompletionItemKind.Unit] = 11,
-			[CompletionItemKind.Value] = 12,
-			[CompletionItemKind.Enum] = 13,
-			[CompletionItemKind.Snippet] = 100,
-			[CompletionItemKind.Text] = 101
-		}
-
-		-- Comparator: prefer kinds per table first
-		local custom_kind_comparator = function(entry1, entry2)
-			local p1 = kind_priorities[entry1:get_kind()] or 1000
-			local p2 = kind_priorities[entry2:get_kind()] or 1000
-			if p1 ~= p2 then
-				return p1 < p2
-			end
-		end
 
 		cmp.setup({
-			-- disable automatic preselection; first visible item will be highlighted
+			completion = { completeopt = 'menu,menuone' },
 			preselect = cmp.PreselectMode.None,
-			-- ensure confirmation replaces by default (keeps behavior predictable)
-			confirmation = { default_behavior = cmp.ConfirmBehavior.Replace },
-			completion = { completeopt = 'menu,menuone,select' },
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered()
-			},
-			formatting = { format = lspkind.cmp_format({
-				mode = 'symbol_text',
-				maxwidth = 50
-			}) },
-			mapping = {
-				['<CR>'] = cmp.mapping(
-					function(fallback)
-						if cmp.visible() then
-							local entry = cmp.get_selected_entry()
-							if entry then
-								cmp.confirm({ select = true })
-							else
-								-- no entry selected: insert newline
-								fallback()
-							end
-						else
-							fallback()
-						end
-					end,
-					{ 'i', 's' }
-				),
-				['<C-Space>'] = cmp.mapping(
-					function()
-						if cmp.visible() then
-							cmp.close()
-						else
-							cmp.complete()
-						end
-					end,
-					{ 'i', 'c' }
-				),
-				['<Down>'] = cmp.mapping.select_next_item(),
-				['<Up>'] = cmp.mapping.select_prev_item()
-			},
+			mapping = cmp.mapping.preset.insert({
+				['<C-k>'] = cmp.mapping.select_prev_item(),
+				['<C-j>'] = cmp.mapping.select_next_item(),
+				['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
+				['<C-Space>'] = cmp.mapping.complete(),
+				['<C-e>'] = cmp.mapping.abort(),
+				['<CR>'] = cmp.mapping.confirm({ select = true })
+			}),
 			sources = cmp.config.sources({ {
 				name = 'nvim_lsp',
 				priority = 1000
@@ -96,15 +34,16 @@ return {
 			} }),
 			sorting = {
 				comparators = {
-					custom_kind_comparator, -- keep your kind priorities first
-					cmp.config.compare.exact, -- exact prefix wins
-					cmp.config.compare.locality, -- proximity/locality (not CamelCase boundary boost)
-					cmp.config.compare.sort_text, -- tie-break lexicographically
-					cmp.config.compare.length, -- shorter names first
-					cmp.config.compare.score, -- fuzzy scoring as fallback
+					cmp.config.compare.exact,
+					cmp.config.compare.score,
+					cmp.config.compare.recently_used,
+					cmp.config.compare.kind,
+					cmp.config.compare.sort_text,
+					cmp.config.compare.length,
 					cmp.config.compare.order
 				}
 			}
 		})
 	end
 }
+
